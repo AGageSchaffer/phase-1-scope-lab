45d2df301b6438b6c19745658200f98902dae047
AustinSchaffer <austinschaffer@gmail.com>
2022-12-22 14:04:48 -0600

    commit tree 224093a8b44b40633899fced9261938f12e0c4d7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Ted'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    const first = 'not ' 
+    const last = 'bob'
+    bestCustomer = first + last
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe ' + 'bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Jeff'
+}
